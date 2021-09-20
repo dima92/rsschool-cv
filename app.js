aca39f89b60014b3b300de50771655f334ae09de
dima92 <dima-tkachenko@mail.ru>
2021-09-20 22:51:57 +0300

    fix: edit template cv

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ import {log} from './log.js'
 const hamburger = document.querySelector('.hamburger')
 const hamburgerLine = document.querySelector('.hamburger__line')
 const navigation = document.querySelector('.navigation')
+const links = document.querySelectorAll('.navigation__link')
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburgerLine.classList.remove('hamburger__line_active')
+    navigation.classList.remove('navigation_hamburger-active')
+  })
+})
 
 hamburger.addEventListener('click', () => {
   hamburgerLine.classList.toggle('hamburger__line_active')
